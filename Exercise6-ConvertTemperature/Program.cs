c9fc6f925f237851f161ac2aef84a35309dafc6c
Deepak Sharma <deepak.dev78@outlook.com>
2017-04-27 22:42:16 +1200

    - convert Celsius to Kelvin and Fahrenheit

--- Exercise6-ConvertTemperature/Program.cs
+++ Exercise6-ConvertTemperature/Program.cs
@@ -8,8 +8,22 @@ namespace Exercise6_ConvertTemperature
 {
     class Program
     {
+        //Write a C# program to convert from celsius degrees to Kelvin and Fahrenheit.
+        // K = celsius + 273
+        // F = celsius * 18 / 10 + 32
         static void Main(string[] args)
         {
-        }
+            Console.Write("Enter temperature in Celssius Degree: ");
+            double temp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            double k = temp + 273.15;
+            double f = temp * 18/10 + 32;
+
+            Console.WriteLine("Temperature in Kelvin is: {0}", k);
+            Console.WriteLine("Temperature in Fahrenheit is: {0}", f);
+
+            Console.ReadLine();
+        } 
     }
 }
